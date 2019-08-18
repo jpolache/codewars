1683febdb41bb48d0e82e83fdb2bdd331f882683
jpolache <jpolache@gmail.com>
2019-08-18 04:15:31 +0000

    2315

--- poal.py
+++ poal.py
@@ -1,7 +1,9 @@
 def partlist(arr):
-     for i, word in enumerate(arr):
+    op = []
+    for i, word in enumerate(arr):
         if i < len(arr)-1:
-            print([[" ".join(arr[:i+1])], [" ".join(arr[i+1:])]])
+            op.append([(" ".join(arr[:i+1])), (" ".join(arr[i+1:]))])
+    print(op)
 
 partlist(["I", "wish", "I", "hadn't", "come"])
 #partlist(["cdIw", "tzIy", "xDu", "rThG"])
