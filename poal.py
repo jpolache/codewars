472d72cd6016e30e43849f6123db7e8941c9d6f9
jpolache <jpolache@gmail.com>
2019-08-18 13:31:26 +0000

    0831

--- poal.py
+++ poal.py
@@ -1,10 +1,15 @@
+import json
+
 def partlist(arr):
     op = []
     for i, word in enumerate(arr):
         if i < len(arr)-1:
             op.append([(" ".join(arr[:i+1])), (" ".join(arr[i+1:]))])
-    print(op)
+    print(json.dumps(op))
 
 partlist(["I", "wish", "I", "hadn't", "come"])
 #partlist(["cdIw", "tzIy", "xDu", "rThG"])
 #partlist(["vJQ", "anj", "mQDq", "sOZ"])
+
+#def partlist(arr):
+#    return [(' '.join(arr[:i]), ' '.join(arr[i:])) for i in range(1, len(arr))]
