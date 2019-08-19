648a4ec9902fd3cb0eb0c1299eb31c70d4243209
jpolache <jpolache@gmail.com>
2019-08-19 20:57:41 +0000

    1557

--- ndn.py
+++ ndn.py
@@ -1,6 +1,14 @@
 def tidyNumber(n):
-    [int(x) for x in str(n)]
-    #pass
+    op = True
+    nlist =  [int(x) for x in str(n)]
+    for i, num in enumerate(nlist):
+        try:
+            if nlist[i] > nlist[i+1]:
+                op = False
+        except:
+            pass
+    return op
+
 
 tidyNumber(9672)
 tidyNumber(2789)
