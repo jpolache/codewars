910b23133105e4994c0a0a5d897b8de7214b8daf
jpolache <jpolache@gmail.com>
2019-08-19 21:34:26 +0000

    1634

--- rdw.py
+++ rdw.py
@@ -2,7 +2,6 @@ def remove_duplicate_words(s):
     #s = list(dict.fromkeys( s.split() ))
     #s = list(dict(s.split()))
     print(set( s.split() ))
-    pass
 
 remove_duplicate_words("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta")
 remove_duplicate_words("my cat is my cat fat")
