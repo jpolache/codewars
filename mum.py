e1734726000ece76a5a8a89aa2f2f17e014dafda
jpolache <jpolache@gmail.com>
2019-08-19 13:59:17 +0000

    0859

--- mum.py
+++ mum.py
@@ -1,8 +1,12 @@
 def accum(s):
     op = ""
     for i, _ in enumerate(range(len(s))):
-        op = op + str(s[i]*(i+1))
-        op = op + "-" 
+        if s[i].upper() not in op.upper():
+            op = op + s[i].upper()
+
+        op = op + s[i].lower()*i
+        if s[-1] not in op:
+            op = op + "-"
         print(op)
 
 accum("ZpglnRxqenU")
