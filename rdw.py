24437b2f3b03f9a6e3c0a01bf590fef6324ede94
jpolache <jpolache@gmail.com>
2019-08-20 11:23:40 +0000

    0623

--- rdw.py
+++ rdw.py
@@ -1,7 +1,9 @@
 def remove_duplicate_words(s):
-    #s = list(dict.fromkeys( s.split() ))
-    #s = list(dict(s.split()))
-    print(set( s.split() ))
+    op = []
+    for item in s.split():
+        if  item not in op:
+            op.append(item)
+    return op
 
 remove_duplicate_words("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta")
 remove_duplicate_words("my cat is my cat fat")
