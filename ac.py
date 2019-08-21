ea5f499bd3fff583c99ef40d621acaa480169ee8
jpolache <jpolache@gmail.com>
2019-08-21 13:38:04 +0000

    0838

--- ac.py
+++ ac.py
@@ -1,11 +1,18 @@
 def capitalize(s):
     op = ""
+    op2 = ""
     print(s)
     mapped = zip(s[::2].upper(), s[1::2].lower())
+    mapped2 = zip(s[::2].lower(), s[1::2].upper())
     for i in mapped:
         for x in i:
             op = op + str(x)
+    for i in mapped2:
+        for x in i:
+            op2 = op2 + str(x)
+
     print(op)
+    print(op2)
 
 capitalize("abcdef")
 capitalize("codewars")
