ae5532b9e1cabeabd34bffc26d33aff5fc846a78
jpolache <jpolache@gmail.com>
2019-08-24 13:58:07 +0000

    0857

--- ftm.py
+++ ftm.py
@@ -1,5 +1,8 @@
 def min_value(digits):
-    print(''.join(str(e) for e in sorted(set(digits))))
+    d_str = ''.join(str(e) for e in digits)
+    tn = (''.join(str(e) for e in sorted(set(digits))))
+    #print(d_str, tn)
+    print(''.join(str(e) for e in digits) == ''.join(str(e) for e in sorted(set(digits))))
 
 min_value([1, 3, 1])
 
