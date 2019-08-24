dd5f6bd99927e19fe8ad6428ca756e6535a21aec
jpolache <jpolache@gmail.com>
2019-08-24 01:03:27 +0000

    2003

--- rm.py
+++ rm.py
@@ -1,8 +1,10 @@
 def multiples(m, n):
     op = []
-    for i, num in ramge(n):
-        op.append(num*(i+1)
-    print("vec!"+op)
+    #print(m, n)
+    for i, num in enumerate(range(m)):
+        #print(str(n), str(i))
+        op.append(n*(i+1))
+    print("vec!" + str(op))
 
 multiples(3, 5) #vec![5, 10, 15]
 multiples(4, 6) #vec![6, 12, 18, 24]
