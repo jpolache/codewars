7843ac5e8fb459d64d3a4aa3673441efc70566ea
jpolache <jpolache@gmail.com>
2019-08-24 00:41:37 +0000

    1941

---
+++ rm.py
@@ -0,0 +1,8 @@
+def multiples(m, n):
+    op = []
+    for i, num in ramge(n):
+        op.append(num*(i+1)
+    print("vec!"+op)
+
+multiples(3, 5) #vec![5, 10, 15]
+multiples(4, 6) #vec![6, 12, 18, 24]
