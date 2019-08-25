dcc95e296c3f10c8b7f4b8034228b23bb585c539
jpolache <jpolache@gmail.com>
2019-08-25 00:47:42 +0000

    1947

--- fsa.py
+++ fsa.py
@@ -1,8 +1,7 @@
 def flatten_and_sort(array):
     my_array = []
-    for x in array:
-        array.append(x))
-    return []
+    [my_array.extend(x) for x in array]
+    print(sorted(my_array))
+    #return []
 
-
-flatten_and_sort([[1, 3, 5], [100], [2, 4, 6]])
+flatten_and_sort([[1, 3, 5], [100], [2, 4, 6]]) #[1, 2, 3, 4, 5, 6, 100]
