956a62a72e3cc235b44dd91b038187b00ac867a9
jpolache <jpolache@gmail.com>
2019-09-14 22:23:46 +0000

    0523

--- ndn.py
+++ ndn.py
@@ -1,7 +1,7 @@
 def tidyNumber(n):
     op = True
     nlist =  [int(x) for x in str(n)]
-    for i, num in enumerate(nlist):
+    for i, _ in enumerate(nlist):
         try:
             if nlist[i] > nlist[i+1]:
                 op = False
