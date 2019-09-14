6a3df8be9aad13681eeddc38d13490bf8fda8791
jpolache <jpolache@gmail.com>
2019-09-14 22:21:11 +0000

    0521

--- odd.py
+++ odd.py
@@ -3,9 +3,9 @@ print(a)
 b = sorted([item for item in a if item%2 == 0])
 odd_int = 0
 for i in range(len(a)):
-    print("a[i]: ", a[i])
+    print("a: ", a)
     if a[i] %2 == 0:
-        print("odd_int: ", [odd_int])
+        print("b: ", b)
         a[i] = b[odd_int]
         odd_int += 1
 
