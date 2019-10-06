9eaa602568f2998f8b847d5e99d07a48a1510c18
jpolache <jpolache@gmail.com>
2019-10-05 23:00:37 -0500

    Update pa.py

--- pa.py
+++ pa.py
@@ -28,8 +28,8 @@ print(pyramid(3)) #, [[1], [1, 1], [1, 1, 1]])
 def pyramid(n):
     return [[1]*x for x in range(1, n+1)]
     
-pyramid(0), [])
-pyramid(1), [[1]])
-pyramid(2), [[1], [1, 1]])
-pyramid(3), [[1], [1, 1], [1, 1, 1]])
+print(pyramid(0)) #, [])
+print(pyramid(1)) #, [[1]])
+print(pyramid(2)) #, [[1], [1, 1]])
+print(pyramid(3)) #, [[1], [1, 1], [1, 1, 1]])   
 '''
